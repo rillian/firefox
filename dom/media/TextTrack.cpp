6720ead88e82d13dee5f75104b10576ebbe2ca9c
Dale Karp <me@dale.io>
2013-01-31 18:33:05 -0500

    current progress check in. worked on constructor, added new properties and methods for TextTrack

--- dom/media/TextTrack.cpp
+++ dom/media/TextTrack.cpp
@@ -31,14 +31,31 @@ NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(TextTrack)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
 
-TextTrack::TextTrack(nsISupports *aParent) : mParent(aParent)
+TextTrack::TextTrack(nsISupports *aParent,
+    const nsAString& aKind)
+    const nsAString& aLabel,
+    const nsAString& aLanguage) :
+  mParent(aParent),
+  mKind(aKind),
+  mLabel(aLabel),
+  mLanguage(aLanguage)
+  //XXX:mMode(WEBVTT_MODE_HIDDEN),
+  //XXX:mCueList(new cue list goes here),
+  //XXX:mActiveCueList(new active cue list)
 {
+  //XXX: any asserts done here?
+  //XXX: dom spec says to set
+  //certain strings as default
+  //if they are empty. label
+  //and language are optional
   SetIsDOMBinding();
+  //XXX: populate both cue lists
 }
 
 TextTrack::~TextTrack()
 {
   mParent = nullptr;
+  //XXX: anything else need to be destroyed?
 }
 
 JSObject*
@@ -46,6 +63,72 @@ TextTrack::WrapObject(JSContext* aCx, JSObject* aScope,
 		      bool* aTriedToWrap)
 {
   return TextTrackBinding::Wrap(aCx, aScope, this, aTriedToWrap);
+}
+
+nsISupports*
+TextTrack::GetParentObject()
+{
+  return mParent;
+}
+
+void
+TextTrack::GetKind(nsAString& aKind)
+{
+  aKind = mKind;
+}
+
+void
+TextTrack::GetLabel(nsAString& aLabel)
+{
+  aLabel = mLabel;
+}
+
+void
+TextTrack::GetLanguage(nsAString& aLanguage)
+{
+  aLanguage = mLanguage;
+}
+
+void
+TextTrack::GetInBandMetadataTrackDispatchType(nsAString& aType)
+{
+  aType = mType;
+}
+
+TextTrackCueList*
+TextTrack::GetCues()
+{
+  return mCueList;
+}
+
+TextTrackCueList*
+TextTrack::GetActiveCues()
+{
+  return mActiveCueList;
+}
+
+void
+TextTrack::AddCue(TextTrackCue& cue)
+{
+  //XXX: if cue exists, remove
+  //XXX: add cue
+  //mCueList->addCue(cue);
+  
+}
+
+void
+TextTrack::RemoveCue(TextTrackCue& cue)
+{
+  //XXX: if cue does not exists throw
+  //a NotFoundError exception
+  //XXX: remove cue
+  //mCueList->removeCue(cue);
+}
+
+void
+TextTrack::CueChanged(TextTrackCue& cue)
+{
+  //XXX: cue changed handling
 }
 
 } // namespace dom
