19cd18971377d288288e28be409a829b08635415
Dale Karp <me@dale.io>
2013-02-26 00:13:11 -0500

    removed/replaced 3 macros with one

--- dom/media/TextTrackCue.cpp
+++ dom/media/TextTrackCue.cpp
@@ -2,25 +2,13 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 #include "TextTrackCue.h"
 #include "mozilla/dom/TextTrackCueBinding.h"
 
 namespace mozilla {
 namespace dom {
 
-NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(TextTrackCue)
-  NS_IMPL_CYCLE_COLLECTION_UNLINK_PRESERVED_WRAPPER
-NS_IMPL_CYCLE_COLLECTION_UNLINK_END
-
-NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(TextTrackCue)
-  NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mGlobal)
-  NS_IMPL_CYCLE_COLLECTION_TRAVERSE_SCRIPT_OBJECTS
-NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
-
-NS_IMPL_CYCLE_COLLECTION_TRACE_BEGIN(TextTrackCue)
-  NS_IMPL_CYCLE_COLLECTION_TRACE_PRESERVED_WRAPPER
-NS_IMPL_CYCLE_COLLECTION_TRACE_END
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_1(TextTrackCue, mGlobal)
 
 NS_IMPL_CYCLE_COLLECTING_ADDREF(TextTrackCue)
 NS_IMPL_CYCLE_COLLECTING_RELEASE(TextTrackCue)
@@ -30,7 +18,7 @@ NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(TextTrackCue)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
 
-TextTrackCue::TextTrackCue(nsISupports *aGlobal,
+TextTrackCue::TextTrackCue(nsISupports* aGlobal,
                            const double aStartTime,
 	                         const double aEndTime,
 	                         const nsAString& aText)
