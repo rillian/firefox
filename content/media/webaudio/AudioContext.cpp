e8b96dd1255a00bbfceb1af8c1644d3e4bdd8295
Ehsan Akhgari <ehsan@mozilla.com>
2013-06-24 10:40:58 -0700

    Bug 865253 - Part 1: Implement the DOM bindings for OscillatorNode; r=roc

--- content/media/webaudio/AudioContext.cpp
+++ content/media/webaudio/AudioContext.cpp
@@ -28,6 +28,7 @@
 #include "WaveShaperNode.h"
 #include "PeriodicWave.h"
 #include "ConvolverNode.h"
+#include "OscillatorNode.h"
 #include "nsNetUtil.h"
 
 namespace mozilla {
@@ -329,6 +330,14 @@ AudioContext::CreateBiquadFilter()
   nsRefPtr<BiquadFilterNode> filterNode =
     new BiquadFilterNode(this);
   return filterNode.forget();
+}
+
+already_AddRefed<OscillatorNode>
+AudioContext::CreateOscillator()
+{
+  nsRefPtr<OscillatorNode> oscillatorNode =
+    new OscillatorNode(this);
+  return oscillatorNode.forget();
 }
 
 already_AddRefed<PeriodicWave>
