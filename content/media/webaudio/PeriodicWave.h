8af7867ff8a87facbe9e3db99577ee3965027b7c
Ralph Giles <giles@mozilla.com>
2013-08-28 15:39:26 -0700

    Bug 865256 - Part 4: Implement custom waveforms. r=ehsan
    Copy the periodicWave data into local storage. Add a
    SetBuffer implementation to send the buffer data to
    the OscillatorNodeEngine.
    
    Change the constructor implemenation to only take one
    length, since both arrays must be the same size.

--- content/media/webaudio/PeriodicWave.h
+++ content/media/webaudio/PeriodicWave.h
@@ -7,16 +7,19 @@
 #ifndef PeriodicWave_h_
 #define PeriodicWave_h_
 
+#include "AudioContext.h"
 #include "nsWrapperCache.h"
 #include "nsCycleCollectionParticipant.h"
 #include "mozilla/Attributes.h"
 #include "EnableWebAudioCheck.h"
-#include "AudioContext.h"
 #include "nsAutoPtr.h"
 
 namespace mozilla {
 
+class ThreadSharedFloatArrayBufferList;
+
 namespace dom {
+
 
 class PeriodicWave MOZ_FINAL : public nsWrapperCache,
                                public EnableWebAudioCheck
@@ -24,9 +27,8 @@ class PeriodicWave MOZ_FINAL : public nsWrapperCache,
 public:
   PeriodicWave(AudioContext* aContext,
                const float* aRealData,
-               uint32_t aRealDataLength,
                const float* aImagData,
-               uint32_t aImagDataLength);
+               uint32_t aLength);
 
   NS_INLINE_DECL_CYCLE_COLLECTING_NATIVE_REFCOUNTING(PeriodicWave)
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_NATIVE_CLASS(PeriodicWave)
@@ -39,8 +41,20 @@ public:
   virtual JSObject* WrapObject(JSContext* aCx,
                                JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
 
+  already_AddRefed<ThreadSharedFloatArrayBufferList>
+    GetThreadSharedBuffer();
+
+  int32_t DataLength() const
+  {
+    return mLength;
+  }
+
 private:
   nsRefPtr<AudioContext> mContext;
+
+  nsAutoPtr<float> mRealData;
+  nsAutoPtr<float> mImagData;
+  int32_t mLength;
 };
 
 }
