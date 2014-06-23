1d7e939d69ce3622456f169f4a286b98aa5188b2
Ralph Giles <giles@mozilla.com>
2014-06-23 13:03:07 -0700

    Bug 941296 - Platform decoder for OS X. r=cpearce
    Implement an h.264 decoder using Apple's VideoToolbox framework.
    Based on gstreamer's vtdec because there's very little documentation
    for this API beyond the framework's header file.
    
    AAC decoding is stubbed to use the BlankDecoderModule's 440A tone.
    
    Reuse the MOZ_APPLEMEDIA define from the mp3 decoder.
    I think I want OSXVTMediaDataDecoder instead?
    
    It appears neither the moz.build nor the Makefile.in LDFLAGS
    contribute to the link line. So leave in the moz.build where
    it belongs, but that doesn't work yet. See bug 973649.

---
+++ content/media/fmp4/apple/AppleDecoderModule.cpp
@@ -0,0 +1,89 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/Preferences.h"
+#include "mozilla/DebugOnly.h"
+#include "mp4_demuxer/mp4_demuxer.h"
+#include "AppleDecoderModule.h"
+#include "AppleATDecoder.h"
+#include "AppleVTDecoder.h"
+
+namespace mozilla {
+
+extern PlatformDecoderModule* CreateBlankDecoderModule();
+
+bool AppleDecoderModule::sIsEnabled = false;
+
+AppleDecoderModule::AppleDecoderModule()
+{
+}
+
+AppleDecoderModule::~AppleDecoderModule()
+{
+}
+
+/* static */
+void
+AppleDecoderModule::Init()
+{
+  MOZ_ASSERT(NS_IsMainThread(), "Must be on main thread.");
+  sIsEnabled = Preferences::GetBool("media.apple.mp4.enabled", false);
+  if (!sIsEnabled) {
+    return;
+  }
+
+  // TODO: dlopen VideoToolbox.framework if it's available.
+  // If it's not, disable ourselves.
+}
+
+nsresult
+AppleDecoderModule::Startup()
+{
+  if (!sIsEnabled) {
+    return NS_ERROR_FAILURE;
+  }
+  // TODO: set up shared decoder state.
+  NS_WARNING("AppleDecoderModule::Startup()");
+  return NS_OK;
+}
+
+nsresult
+AppleDecoderModule::Shutdown()
+{
+  MOZ_ASSERT(NS_IsMainThread(), "Must be on main thread.");
+
+  // TODO: shut down shared state.
+  NS_WARNING("AppleDecoderModule::Shutdown()");
+  return NS_OK;
+}
+
+MediaDataDecoder*
+AppleDecoderModule::CreateH264Decoder(const mp4_demuxer::VideoDecoderConfig& aConfig,
+                                    mozilla::layers::LayersBackend aLayersBackend,
+                                    mozilla::layers::ImageContainer* aImageContainer,
+                                    MediaTaskQueue* aVideoTaskQueue,
+                                    MediaDataDecoderCallback* aCallback)
+{
+  NS_WARNING("Creating h264 decoder on OS X");
+  return new AppleVTDecoder(aConfig, aVideoTaskQueue, aCallback, aImageContainer);
+}
+
+MediaDataDecoder*
+AppleDecoderModule::CreateAACDecoder(const mp4_demuxer::AudioDecoderConfig& aConfig,
+                                   MediaTaskQueue* aAudioTaskQueue,
+                                   MediaDataDecoderCallback* aCallback)
+{
+  NS_WARNING("HACK: using a BlankDecoderModule for AAC");
+  if (!mBlankDecoder) {
+    mBlankDecoder = CreateBlankDecoderModule();
+  }
+  return mBlankDecoder->CreateAACDecoder(aConfig, aAudioTaskQueue, aCallback);
+
+  NS_WARNING("Creating AAC decoder on OS X");
+  return new AppleATDecoder(aConfig, aAudioTaskQueue, aCallback);
+}
+
+} // namespace mozilla
--- media/libstagefright/binding/include/mp4_demuxer/mp4_demuxer.h
+++ media/libstagefright/binding/include/mp4_demuxer/mp4_demuxer.h
@@ -58,6 +58,7 @@ private:
 
   nsAutoPtr<StageFrightPrivate> mPrivate;
 };
-}
 
-#endif
+} // namespace mozilla
+
+#endif // MP4_DEMUXER_H_
