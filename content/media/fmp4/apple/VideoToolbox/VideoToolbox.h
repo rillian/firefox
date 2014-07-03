42b45b268aa9f6423ff710bc55fe969d04efc987
Ralph Giles <giles@mozilla.com>
2014-07-03 16:04:00 -0700

    Bug 941296 - Platform decoder for OS X. r=cpearce
    Implement an h.264 decoder using Apple's VideoToolbox framework.
    Based on gstreamer's vtdec because there's very little documentation
    for this API beyond the framework's header file.
    
    AAC decoding is stubbed to use the BlankDecoderModule's 440A tone.
    
    Reuse the MOZ_APPLEMEDIA define from the mp3 decoder.
    
    Includes a stub header with the VideoToolbox API calls
    so we can build on older MacOS where the framework isn't
    available. We dynamically open the framework dylib at runtime
    if it's available.

---
+++ content/media/fmp4/apple/VideoToolbox/VideoToolbox.h
@@ -0,0 +1,55 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ *  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ *   * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Stub header for VideoToolbox framework API.
+// We include our own copy so we can build on MacOS versions
+// where it's not available.
+
+#ifdef mozilla_VideoToolbox_VideoToolbox_h
+#define mozilla_VideoToolbox_VideoToolbox_h
+
+typedef struct OpaqueVTDecompressionSession* VTDecompressionSessionRef;
+typedef void (*VTDecompressionOutputCallback)(
+    void*,
+    void*,
+    OSStatus,
+    VTDecodeInfoFlags,
+    CVImageBufferRef,
+    CMTime,
+    CMTime
+);
+typedef struct VTDecompressionOutputCallbackRecord {
+  VTDecompressionOutputCallback decompressionOutputCallback,
+  void*                         decompressionOutputRefCon
+} VTDecompressionOutputCallbackRecord;
+
+typedef uint32_t VTDecodeFrameFlags;
+typedef uint32_t VTDecodeInfoFlags;
+
+OSStatus
+VTDecompressionSessionCreate(
+    CFAllocatorRef,
+    CMVideoFormatDescriptionRef,
+    CFDictionaryRef,
+    const VTDecompressionOutputCallbackRecord*,
+    VTDecompressionSessionRef*
+);
+
+OSStatus
+VTDecompressionSessionDecodeFrame(
+    VTDecompressionSessionRef,
+    CMSampleBufferRef,
+    VTDecodeFrameFlags,
+    void*,
+    VTDecodeInfoFlags
+);
+
+void
+VTDecompressionSessionInvalidate(
+    VTDecompressionSessionRef
+);
+
+#endif // mozilla_VideoToolbox_VideoToolbox_h
