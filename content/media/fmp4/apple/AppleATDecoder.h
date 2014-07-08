45672c417d8dca10e497bb2706096985c7300a07
Ralph Giles <giles@mozilla.com>
2014-07-08 13:22:17 -0700

    Bug 941296 - AAC platform decoder for OS X. r=edwin
    WIP Stub out Apple decoder for mpeg audio based on AudioToolbox.

---
+++ content/media/fmp4/apple/AppleATDecoder.h
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_AppleATDecoder_h
+#define mozilla_AppleATDecoder_h
+
+#include <AudioToolbox/AudioToolbox.h>
+#include "PlatformDecoderModule.h"
+#include "mozilla/RefPtr.h"
+#include "mozilla/ReentrantMonitor.h"
+#include "nsIThread.h"
+
+namespace mozilla {
+
+class MediaTaskQueue;
+class MediaDataDecoderCallback;
+
+class AppleATDecoder : public MediaDataDecoder {
+public:
+  AppleATDecoder(const mp4_demuxer::AudioDecoderConfig& aConfig,
+                 MediaTaskQueue* aVideoTaskQueue,
+                 MediaDataDecoderCallback* aCallback);
+  ~AppleATDecoder();
+
+  virtual nsresult Init() MOZ_OVERRIDE;
+  virtual nsresult Input(mp4_demuxer::MP4Sample* aSample) MOZ_OVERRIDE;
+  virtual nsresult Flush() MOZ_OVERRIDE;
+  virtual nsresult Drain() MOZ_OVERRIDE;
+  virtual nsresult Shutdown() MOZ_OVERRIDE;
+
+private:
+  const mp4_demuxer::AudioDecoderConfig& mConfig;
+  RefPtr<MediaTaskQueue> mTaskQueue;
+  MediaDataDecoderCallback* mCallback;
+  AudioConverterRef mConverter;
+  AudioFileStreamID mStream;
+};
+
+} // namespace mozilla
+
+#endif // mozilla_AppleATDecoder_h
