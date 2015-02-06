77195e36c5a1a7752ba83294768c319e33a76861
Ralph Giles <giles@mozilla.com>
2015-02-06 14:07:10 -0800

    Bump libvpx version.
    No upstream release. This is the revision currently used by chromium.
    
    Results of running ./update.py --ndk ~/android/android-ndk-r9 --commit
    afad1a84c15b9af8298a37c0fa449e0af40931fd

--- media/libvpx/vp9/common/vp9_systemdependent.h
+++ media/libvpx/vp9/common/vp9_systemdependent.h
@@ -12,14 +12,12 @@
 #define VP9_COMMON_VP9_SYSTEMDEPENDENT_H_
 
 #ifdef _MSC_VER
-# include <math.h>  // the ceil() definition must precede intrin.h
 # if _MSC_VER > 1310 && (defined(_M_X64) || defined(_M_IX86))
 #  include <intrin.h>
 #  define USE_MSC_INTRIN
 # endif
-# if _MSC_VER < 1900
-#  define snprintf _snprintf
-# endif
+# include <math.h>
+# define snprintf _snprintf
 #endif
 
 #ifdef __cplusplus
@@ -77,6 +75,9 @@ static INLINE int get_msb(unsigned int n) {
   return log;
 }
 #endif
+
+struct VP9Common;
+void vp9_machine_specific_config(struct VP9Common *cm);
 
 #ifdef __cplusplus
 }  // extern "C"
